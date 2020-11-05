92b3cf1dbcf615ee1523615f29644e84a96a9592
buzmakovva <anastasija644@gmail.com>
2020-11-05 16:12:32 +0300

    first

--- practice_js/app.js
+++ practice_js/app.js
@@ -1,22 +1,4 @@
-/*const person = {
-	name: 'Nasty',
-	age: 25,
-	lang:['ru','en'],
-	husband: false,
-	greet(){
-		console.log('my name is', this.name)
-	},
-}
 
-const myPerson = {
-	keys() {
-		Object.keys(this).forEach((key)=>{
-			console.log( `'${key}:' ${this[key]}`)
-			
-		})
-	},
-}
-myPerson.keys.call(myPerson)*/
 const floor = document.querySelector('.floor')
 const walls = document.querySelector('.walls')
 const meet = document.querySelector('.meet')
@@ -27,7 +9,3 @@ calculate.onclick = () => {
 	console.log(valueMeet) 
 }
 
-
-/*const floorR = document.getElementById('floorRoom')
-console.dir(floorR)
-*/
